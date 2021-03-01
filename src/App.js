0b9c2e54dff35831a620b48900e212911f9b685b
Sabieena <sabina.bhattrai2015@gmail.com>
2021-03-01 13:31:58 +0545

    usestate with Array

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import HookCounter from './component/HookCounter'
 import Counter from './component/Counter';
 import ObjectHook from './component/ObjectHook';
+import StateArray from './component/StateArray';
 
 function App() {
   return (
     <div className="App">
       {/* <HookCounter /> */}
       {/* <Counter /> */}
-      <ObjectHook />
+      {/* <ObjectHook /> */}
+      <StateArray />
     </div>
   );
 }
