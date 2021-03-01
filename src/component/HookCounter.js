06bb92d65f98747a0d3aa92757932733472158e2
Sabieena <sabina.bhattrai2015@gmail.com>
2021-03-01 13:40:01 +0545

    some explanation

--- src/component/HookCounter.js
+++ src/component/HookCounter.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 
 function HookCounter() {
+    // first variable is the actal state and second variable is  a function i.e update the state by providing a new state
     const[count, setCount] = useState(0)
     return (
         <div>
--- src/component/StateArray.js
+++ src/component/StateArray.js
@@ -2,11 +2,11 @@ import React, {useState} from 'react'
 
 function StateArray() {
     const[items, setItems] = useState([])
-
+// while dealing with objects and arrays, always make sure to spread your state variable and then call setter function
     const addItem = () => {
         setItems([...items, {
             id: items.length,
-            value: Math.floor(Math.random() * 10)  + 1 
+            value: Math.floor(Math.random() * 10)   + 1 
         }])
 
     }
