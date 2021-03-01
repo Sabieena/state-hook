e76f22e620e6b65b1b3ef2fc048425e84892e050
sabina <sabieena>
2021-03-01 11:54:09 +0545

    hooks with object

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import HookCounter from './component/HookCounter'
 import Counter from './component/Counter';
+import ObjectHook from './component/ObjectHook';
 
 function App() {
   return (
     <div className="App">
       {/* <HookCounter /> */}
-      <Counter />
+      {/* <Counter /> */}
+      <ObjectHook />
     </div>
   );
 }
---
+++ src/component/ObjectHook.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react'
+
+function ObjectHook() {
+    const[name, setName] = useState({firstname: '', lastname: ''})
+    return (
+        <form>
+            <div>
+                <input type = "text" value = {name.firstname} onChange = {e => setName({...name, firstname: e.target.value })} />
+
+                <input type = "text" value = {name.lastname} onChange = {e => setName({...name, lastname: e.target.value})} />
+                <h2>Your firstname is {name.firstname}</h2>
+                <h2>Your lastname is {name.lastname}</h2>
+            </div>
+        </form>
+    )
+}
+
+export default ObjectHook
